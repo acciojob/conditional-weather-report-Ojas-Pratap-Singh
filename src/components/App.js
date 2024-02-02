2d689a1d76ea8637795d2850a807d096913b5fdc
Ojas <singhojas3299@gmail.com>
2024-02-02 14:08:40 +0530

    done

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,21 @@
-
 import React from "react";
-import './../styles/App.css';
+import "./../styles/App.css";
 
 const App = () => {
+  const weather = { temperature: 25, conditions: "Sunny" };
   return (
     <div>
-        {/* Do not remove the main div */}
+      {weather.temperature > 20 ? (
+        <p style={{ color: "red" }}>
+          {weather.temperature} <br /> {weather.conditions}
+        </p>
+      ) : (
+        <p style={{ color: "blue" }}>
+          {weather.temperature} <br /> {weather.conditions}
+        </p>
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
